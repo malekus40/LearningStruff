9a83ad84927250fab2f7a3947f5ad093dd584f0e
Abdul Malek <malekus40@gmail.com>
2023-12-13 20:40:49 -0800

    keypress, keyup and keydown. learned to perform actions when input is entered

--- EventsOnMultipleElem/app.js
+++ EventsOnMultipleElem/app.js
@@ -10,7 +10,7 @@ const colors = [
 ];
 
 
-const printColor = function() {
+const printColor = function (evt) {
     const h1 = document.querySelector('h1')
     h1.style.color = this.style.backgroundColor;
 };
@@ -22,5 +22,34 @@ for (let color of colors) {
     box.style.backgroundColor = color;
     box.classList.add('box');
     container.appendChild(box);
-    box.addEventListener('click',printColor);
+    box.addEventListener('click', printColor);
 }
+
+document.body.addEventListener('keypress', function (e) {
+    // console.log(e);
+})
+
+const input = document.querySelector("#username");
+input.addEventListener('keydown', function(e){
+    console.log("key down");
+});
+input.addEventListener('keyup', function(e){
+    console.log("key up");
+});
+
+input.addEventListener('keypress', function(e){
+    console.log("key press");
+});
+
+const addItemInput = document.querySelector('#addItem');
+const itemsUL = document.querySelector('#items');
+addItemInput.addEventListener('keypress', function(e){
+    if (e.key === 'Enter'){
+        if (!this.value) return;
+        const newItemText = this.value;
+        const newItem = document.createElement('li');
+        newItem.innerText = newItemText;
+        itemsUL.appendChild(newItem);
+        this.value = '';
+    }
+});
