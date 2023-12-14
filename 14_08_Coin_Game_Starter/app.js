e63e2dead650609bb51de18eadfa93ada7abf238
Abdul Malek <malekus40@gmail.com>
2023-12-14 03:31:57 -0800

    mini project collecting coins

---
+++ 14_08_Coin_Game_Starter/app.js
@@ -0,0 +1,51 @@
+function isTouching(a, b) {
+	const aRect = a.getBoundingClientRect();
+	const bRect = b.getBoundingClientRect();
+
+	return !(
+		aRect.top + aRect.height < bRect.top ||
+		aRect.top > bRect.top + bRect.height ||
+		aRect.left + aRect.width < bRect.left ||
+		aRect.left > bRect.left + bRect.width
+	);
+}
+
+const avatar = document.querySelector("#player");
+
+window.addEventListener('keyup', function(e){
+	if (e.key === 'ArrowDown' || e.key === 'Down'){
+		moveVertical(avatar, 50);
+	} else if (e.key === 'ArrowUp' || e.key === 'Up'){
+		moveVertical(avatar, -50);
+	}else  if (e.key === 'ArrowRight' || e.key === 'Right'){
+		moveHoriz(avatar, 50);
+		avatar.style.transform = 'scale(1,1)';
+	}else  if (e.key === 'ArrowLeft' || e.key === 'Left'){
+		moveHoriz(avatar, -50);
+		avatar.style.transform = 'scale(-1,1)';
+	}
+	
+	if (isTouching(avatar,coin)) moveCoin();
+});
+
+const moveVertical = (element, amount) => {
+	const currTop = extractPos(element.style.top);
+	element.style.top = `${currTop + amount}px`;
+}
+const moveHoriz = (element, amount) => {
+	const currLeft = extractPos(element.style.left);
+	element.style.left = `${currLeft + amount}px`;
+}
+const extractPos = (pos) => {
+	if (!pos) return 100;
+	return parseInt(pos.slice(0,-2));
+
+};
+
+const moveCoin = () => {
+	const x = Math.floor(Math.random() * window.innerWidth);
+	const y = Math.floor(Math.random() * window.innerHeight);
+	coin.style.top = `${x}px`;
+	coin.style.left = `${y}px`;
+}
+moveCoin();
