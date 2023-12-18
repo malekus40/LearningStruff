aeebaa5823c2008e0793ff06fe4fddacf723a25a
Abdul Malek <malekus40@gmail.com>
2023-12-18 13:57:12 -0800

    learned about promise chaining

--- CallBack/app.js
+++ CallBack/app.js
@@ -6,20 +6,130 @@ const btn = document.querySelector('button');
 //     },1000);
 // }, 1000);
 
-const moveX = (element, amount, delay, onSuccess, onFailure) => {
-    const bodyBoundary = document.body.clientWidth;
-    const elRight = element.getBoundingClientRect().right;
-    const currLeft = element.getBoundingClientRect().left;
-    if (elRight + amount > bodyBoundary) {
-        onFailure();
-    }
-    else {
-        setTimeout(() => {
-            element.style.transform = `translateX(${currLeft + amount}px)`;
-            onSuccess();
-        }, delay);
-    }
+const moveX = (element, amount, delay) => {
+    return new Promise((resolve, reject) => {
+        const bodyBoundary = document.body.clientWidth;
+        const elRight = element.getBoundingClientRect().right;
+        const currLeft = element.getBoundingClientRect().left;
+        if (elRight + amount > bodyBoundary) {
+            reject({bodyBoundary, elRight, amount});
+        }
+        else {
+            setTimeout(() => {
+                element.style.transform = `translateX(${currLeft + amount}px)`;
+                resolve();
+            }, delay);
+        }
+    });
+
 };
+
+moveX(btn, 10, 50)
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    .then(() => moveX(btn, 10, 15))
+    
+    .catch((res) => {
+    console.log(`Body is ${res.bodyBoundary}px wide`);
+    console.log(`Element is at ${res.elRight}px, ${res.amount}px is too large!`);
+});
 
 // moveX(btn, 100, 1000, () => {
 //     moveX(btn, 100, 1000, () => {
@@ -41,6 +151,4 @@ const moveX = (element, amount, delay, onSuccess, onFailure) => {
 // });
 
 
-
-requestAnimationFrame(())
 
