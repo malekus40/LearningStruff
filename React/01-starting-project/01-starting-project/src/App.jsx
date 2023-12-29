381e77c29fd143de083e052c0c6f472a7d4fc509
Abdul Malek <malekus40@gmail.com>
2023-12-29 11:50:25 -0800

    learned about components and applying them

--- React/01-starting-project/01-starting-project/src/App.jsx
+++ React/01-starting-project/01-starting-project/src/App.jsx
@@ -1,20 +1,30 @@
-function Header() {
-  return (<header>
-    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-    <h1>React Essentials</h1>
-    <p>
-      Fundamental React concepts you will need for almost any app you are
-      going to build!
-    </p>
-  </header>);
-}
+import componentsImg from './assets/components.png';
+import Header from './components/Header';
+import CoreConcept from './components/CoreConcept';
+import { CORE_CONCEPTS } from './data';
+
+
+
 
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept {...CORE_CONCEPTS[0]}/>
+          <CoreConcept {...CORE_CONCEPTS[1]}/>
+          <CoreConcept {...CORE_CONCEPTS[2]}/>
+          <CoreConcept {...CORE_CONCEPTS[3]}/>
+          
+          
+          
+        </ul>
+        </section>
+        
+
       </main>
     </div>
   );
