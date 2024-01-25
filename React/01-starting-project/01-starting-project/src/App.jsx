9509254053c9cd1984bde35344c52d20add35af3
Abdul Malek <malekus40@gmail.com>
2024-01-25 01:33:26 -0800

    events hangling and managin states

--- React/01-starting-project/01-starting-project/src/App.jsx
+++ React/01-starting-project/01-starting-project/src/App.jsx
@@ -1,30 +1,38 @@
 import componentsImg from './assets/components.png';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept';
 import { CORE_CONCEPTS } from './data';
-
-
-
+import TabButton from './components/TabButton';
 
 function App() {
+  let tabConstent = 'Please click a button';
+  function handleSelect(selectedButton) {
+
+    console.log(selectedButton);
+  }
   return (
     <div>
       <Header></Header>
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
-        <ul>
-          <CoreConcept {...CORE_CONCEPTS[0]}/>
-          <CoreConcept {...CORE_CONCEPTS[1]}/>
-          <CoreConcept {...CORE_CONCEPTS[2]}/>
-          <CoreConcept {...CORE_CONCEPTS[3]}/>
-          
-          
-          
-        </ul>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
         </section>
-        
-
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('component')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {tabConstent};
+        </section>
       </main>
     </div>
   );
--- React/01-starting-project/01-starting-project/src/components/Header/Header.jsx
+++ React/01-starting-project/01-starting-project/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import reactImg from '../assets/react-core-concepts.png';
+import reactImg from '../../assets/react-core-concepts.png';
 const reactDescriptions = ['Fundamental','Crucial', 'Core'];
 import './Header.css';
 
